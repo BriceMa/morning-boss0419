a901a08acfe212f39d7d08ac938db7636e97f4bb
Brice <>
2019-04-01 10:34:22 +0200

    Initialize project on git

---
+++ morning-boss0419/Model/User.swift
@@ -0,0 +1,31 @@
+//
+//  User.swift
+//  morning-boss0419
+//
+//  Created by Brice on 01/04/2019.
+//  Copyright © 2019 Brice Mangeat. All rights reserved.
+//
+
+import Foundation
+
+class User {
+    private var _prenom: String
+    private var _nom: String
+    private var _mail: String
+    private var _tel: String
+    private var _password: String
+    
+    var prenom: String {return _prenom}
+    var nom: String {return _nom}
+    var mail: String {return _mail}
+    var tel: String {return _tel}
+    var password: String {return _password}
+    
+    init(prenom: String, nom: String, mail:String, tel: String, password: String) {
+        _prenom = prenom
+        _nom = nom
+        _mail = mail
+        _tel = tel
+        _password = password
+    }
+}
---
+++ morning-boss0419Tests/morning_boss0419Tests.swift
@@ -0,0 +1,34 @@
+//
+//  morning_boss0419Tests.swift
+//  morning-boss0419Tests
+//
+//  Created by Brice on 01/04/2019.
+//  Copyright © 2019 Brice Mangeat. All rights reserved.
+//
+
+import XCTest
+@testable import morning_boss0419
+
+class morning_boss0419Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
