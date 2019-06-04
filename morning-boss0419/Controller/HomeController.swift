ea2703a539a0d5ee4cd2ad43fd09a9dc655d6587
Brice <>
2019-06-04 20:01:26 +0200

    add data to homepage

--- morning-boss0419/Controller/HomeController.swift
+++ morning-boss0419/Controller/HomeController.swift
@@ -7,14 +7,40 @@ class HomeController: UIViewController {
     @IBOutlet var container: UIView!
     @IBOutlet weak var selectButton: UIImageView!
     @IBOutlet weak var ellipseBg: UIImageView!
+    @IBOutlet weak var homeBtn: UIButton!
+    
+    @IBOutlet weak var menuIV: UIImageView!
+    @IBOutlet weak var prod1Lbl: UILabel!
+    @IBOutlet weak var prod2Lbl: UILabel!
+    @IBOutlet weak var prod3Lbl: UILabel!
+    
+    
     
     private var _segueDeclinateMenu = "declinateMenu"
     private var _segueValidateMenu = "validateMenu"
     
+
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        homeBtn.isEnabled = false
+        homeBtn.alpha = 0.5
         selectButton.isUserInteractionEnabled = true
+        
+        MenuService.retrieveMenus(success: { menus in
+            self.prod1Lbl.text = menus[0].products?[0].name
+            self.prod2Lbl.text = menus[0].products?[1].name
+            self.prod3Lbl.text = menus[0].products?[2].name
+            self.menuIV.download(menus[0].imgMenu?.url ?? "")
+
+        }) { (Error) in
+            print("error")
+        }
+        
+        
+        
+        
     }
 
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
--- morning-boss0419/Controller/MenusController.swift
+++ morning-boss0419/Controller/MenusController.swift
@@ -11,6 +11,10 @@ class MenusController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        collectionView.layer.shadowOffset = CGSize(width: 0, height: 0)
+        collectionView.layer.shadowColor = UIColor(displayP3Red: 129, green: 129, blue: 129, alpha: 0.1).cgColor
+        collectionView.layer.shadowRadius = 15
+        
         MenuService.retrieveMenus(success: { menus in
 
             self.menus = menus
